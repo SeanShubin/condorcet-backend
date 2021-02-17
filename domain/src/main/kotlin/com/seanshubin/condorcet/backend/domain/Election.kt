a25d50abffaa1df83a8a59fa9b0e90aa7cc91c9c
SeanShubin <seanshubin@gmail.com>
2021-02-17 12:05:26 -0800

    domain object for election

---
+++ domain/src/main/kotlin/com/seanshubin/condorcet/backend/domain/Election.kt
@@ -0,0 +1,20 @@
+package com.seanshubin.condorcet.backend.domain
+
+import java.time.Instant
+
+data class Election(
+    val ownerName: String,
+    val name: String,
+    val candidatesNames: List<String> = emptyList(),
+    val eligibleVoterNames: List<String>? = null,
+    val startTime: Instant? = null,
+    val endTime: Instant? = null,
+    val secretBallot: Boolean = true,
+    val whenDoneConfiguring: Instant?,
+    val isTemplate: Boolean = false,
+    val isStarted: Boolean = false,
+    val isFinished: Boolean = false,
+    val canChangeCandidatesAfterDoneConfiguring: Boolean = false,
+    val creatorCanDeleteBallots: Boolean = false,
+    val auditorCanDeleteBallots: Boolean = false
+)
