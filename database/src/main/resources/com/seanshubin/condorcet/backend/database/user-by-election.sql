24389654cbe7c6896507e480063ec75558e32243
SeanShubin <seanshubin@gmail.com>
2022-01-14 10:06:58 -0800

    fixed user-by-election

--- database/src/main/resources/com/seanshubin/condorcet/backend/database/user-by-election.sql
+++ database/src/main/resources/com/seanshubin/condorcet/backend/database/user-by-election.sql
@@ -1,6 +1,6 @@
 select user.name
 from user
      inner join voter on user.id = voter.user_id
-     inner join election on user.id = voter.user_id
+     inner join election on election.id = voter.election_id
 where election.name = ?
 order by user.name
