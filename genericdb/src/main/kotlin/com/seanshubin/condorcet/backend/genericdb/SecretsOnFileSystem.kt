f52a6672de0223cd0b75f06ee26416bf97d947d1
Sean Shubin <sshubin@cj.com>
2022-02-02 21:50:12 -0800

    create password file if it does not exist

--- genericdb/src/main/kotlin/com/seanshubin/condorcet/backend/genericdb/SecretsOnFileSystem.kt
+++ genericdb/src/main/kotlin/com/seanshubin/condorcet/backend/genericdb/SecretsOnFileSystem.kt
@@ -1,7 +1,9 @@
 package com.seanshubin.condorcet.backend.genericdb
 
 import com.seanshubin.condorcet.backend.contract.FilesContract
+import java.nio.file.OpenOption
 import java.nio.file.Path
+import java.nio.file.StandardOpenOption
 
 class SecretsOnFileSystem(
     private val secretsDir: Path,
@@ -9,6 +11,10 @@ class SecretsOnFileSystem(
 ):Secrets {
     override fun databasePassword(): String {
         val path = secretsDir.resolve("database-password.txt")
+        files.createDirectories(secretsDir)
+        if(!files.exists(path)){
+           files.writeString(path, "password-goes-here", StandardOpenOption.CREATE)
+        }
         return files.readString(path)
     }
 }
