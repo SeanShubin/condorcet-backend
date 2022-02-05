04a599c3d538713c1ce9dc099e27e9b99b91e9e7
Sean Shubin <sshubin@cj.com>
2022-02-05 12:41:36 -0800

    updated defaults

--- dependencies/src/main/kotlin/com/seanshubin/condorcet/backend/dependencies/JsonConfigurationLookupFunctions.kt
+++ dependencies/src/main/kotlin/com/seanshubin/condorcet/backend/dependencies/JsonConfigurationLookupFunctions.kt
@@ -7,8 +7,8 @@ class JsonConfigurationLookupFunctions(
     secretsConfiguration: Configuration
 ):ConfigurationLookupFunctions {
     override val lookupDatabaseHost: () -> String = configuration.createStringLookup("database-host", listOf("database", "host"))
-    override val lookupDatabaseUser: () -> String = configuration.createStringLookup("database-user", listOf("database", "user"))
+    override val lookupDatabaseUser: () -> String = configuration.createStringLookup("root", listOf("database", "user"))
     override val lookupDatabasePassword: () -> String = secretsConfiguration.createStringLookup("database-password", listOf("database", "password"))
     override val lookupDatabasePort: () -> Int  = configuration.createIntLookup(3306, listOf("database", "port"))
-    override val lookupServerPort: () -> Int  = configuration.createIntLookup(80, listOf("server", "port"))
+    override val lookupServerPort: () -> Int  = configuration.createIntLookup(8080, listOf("server", "port"))
 }
